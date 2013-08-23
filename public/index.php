5e9ab1d920d091e4c16d539675de087e4faf3822
Jeremy Kendall <jeremy@jeremykendall.net>
2013-08-22 20:56:36 -0500

    Major refactoring after creating replay example, validation middleware

--- public/index.php
+++ public/index.php
@@ -8,44 +8,64 @@
  */
 
 date_default_timezone_set('UTC');
+error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 require '../vendor/autoload.php';
 
 use Example\ApiCredentials;
 use Example\ApiRequestSigner;
 use Example\ApiRequestValidator;
+use Example\Dao\Signature as SignatureDao;
 use Guzzle\Http\Client as GuzzleClient;
+use Guzzle\Http\Exception\BadResponseException;
 use JSend\JSendResponse;
 use QueryAuth\Factory as QueryAuthFactory;
+use Slim\Log;
+use Slim\Slim;
 use Slim\Views\Twig;
 use Slim\Views\TwigExtension;
 
 $config = require_once __DIR__ . '/../config.php';
-$config['mode'] = (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production');
 
-// API credentials
 $credentials = new ApiCredentials($config['api']['key'], $config['api']['secret']);
-
-// QueryAuth Factory for retrieving Server and Client instances
 $factory = new QueryAuthFactory();
 
 // The ApiRequestSigner would be used by an API consumer to sign their requests
 $requestSigner = new ApiRequestSigner($factory->newClient());
 
-// The ApiRequestValidator would be used by an API creator to validation incoming
-// request signatures
+// The ApiRequestValidator would be used by an API creator to validate incoming requests
 $requestValidator = new ApiRequestValidator($factory->newServer());
 
+// Middleware to validate incoming request signatures
+$validateSignature = function(Slim $app, ApiCredentials $credentials, ApiRequestValidator $requestValidator) {
+    return function() use ($app, $credentials, $requestValidator) {
+        $response = $app->response();
+
+        try {
+            if ($requestValidator->isValid($app->request(), $credentials) === false) {
+                $jsend = new JSendResponse('fail', array('message' => 'Invalid signature'));
+                $response->setStatus(403);
+                $response->headers->set('Content-Type', 'application/json');
+                $response->setBody($jsend->encode());
+            }
+        } catch (\Exception $e) {
+            $jsend = new JSendResponse('error', array(), $e->getMessage(), $e->getCode());
+            $response->setStatus(403);
+            $response->headers->set('Content-Type', 'application/json');
+            $response->setBody($jsend->encode());
+        }
+    };
+};
+
 // Prepare app
-$app = new Slim\Slim($config['slim']);
+$app = new Slim($config['slim']);
 
 $app->configureMode('development', function () use ($app) {
-    error_reporting(-1);
-    ini_set('display_errors', 1);
-
     $app->config(array(
         'log.enabled' => true,
-        'log.level' => Slim\Log::DEBUG
+        'log.level' => Log::DEBUG
     ));
 });
 
@@ -74,8 +94,14 @@ $app->get('/get-example', function() use ($app, $credentials, $requestSigner) {
     // Sign request
     $requestSigner->signRequest($request, $credentials);
 
-    $response = $request->send();
+    // Send request
+    try {
+        $response = $request->send();
+    } catch (BadResponseException $bre) {
+        $response = $bre->getResponse();
+    }
 
+    // Render template with data
     $app->render('get.html', array('request' => (string) $request, 'response' => (string) $response));
 });
 
@@ -97,34 +123,73 @@ $app->get('/post-example', function() use ($app, $credentials, $requestSigner) {
     // Sign request
     $requestSigner->signRequest($request, $credentials);
 
-    $response = $request->send();
+    try {
+        $response = $request->send();
+    } catch (BadResponseException $bre) {
+        $response = $bre->getResponse();
+    }
 
     $app->render('post.html', array('request' => (string) $request, 'response' => (string) $response));
 });
+
+$app->map('/replay-example', function () use ($app, $credentials, $requestSigner) {
+
+    // Create request
+    $guzzle = new GuzzleClient('http://query-auth.dev');
+    $request = $guzzle->post('/api/replay-example');
+
+    if ($app->request()->isGet()) {
+
+        $params = array(
+            'name' => 'Ash',
+            'email' => 'ash@s-mart.com',
+            'department' => 'Housewares',
+        );
+
+        foreach ($params as $name => $value) {
+            $request->setPostField($name, $value);
+        }
+
+        // Sign request
+        $requestSigner->signRequest($request, $credentials);
+    }
+
+    if ($app->request()->isPost()) {
+        foreach ($app->request()->post() as $param => $value) {
+            $request->setPostField($param, $value);
+        }
+    }
+
+    try {
+        $response = $request->send();
+    } catch (BadResponseException $bre) {
+        $response = $bre->getResponse();
+    }
+
+    $app->render('replay.html', array(
+        'request' =>  (string) $request, 
+        'response' => (string) $response, 
+        'postFields' => $request->getPostFields(),
+    ));
+})->via('GET', 'POST');
 
 /**
  * Validates a signed GET request and, if the request is valid, returns a
  * famous mangled phrase
  */
-$app->get('/api/get-example', function () use ($app, $credentials, $requestValidator) {
+$app->get('/api/get-example', $validateSignature($app, $credentials, $requestValidator), function () use ($app) {
+    
+    $response = $app->response();
 
-    try {
-        // Validate the request signature
-        $isValid = $requestValidator->isValid($app->request(), $credentials);
-
-        if ($isValid) {
-            $mistakes = array('necktie', 'neckturn', 'nickle', 'noodle');
-            $format = 'Klaatu... barada... n... %s!';
-            $data = array('message' => sprintf($format, $mistakes[array_rand($mistakes)]));
-            $jsend = new JSendResponse('success', $data);
-        } else {
-            $jsend = new JSendResponse('fail', array('message' => 'Invalid signature'));
-        }
-    } catch (\Exception $e) {
-        $jsend = new JSendResponse('error', array(), $e->getMessage());
+    if ($response->isClientError()) {
+        return $response;
     }
 
-    $response = $app->response();
+    $mistakes = array('necktie', 'neckturn', 'nickle', 'noodle');
+    $format = 'Klaatu... barada... n... %s!';
+    $data = array('message' => sprintf($format, $mistakes[array_rand($mistakes)]));
+    $jsend = new JSendResponse('success', $data);
+
     $response['Content-Type'] = 'application/json';
     echo $jsend->encode();
 });
@@ -133,37 +198,80 @@ $app->get('/api/get-example', function () use ($app, $credentials, $requestValid
  * Validates a signed POST request and, if the request is valid, mimics creating
  * a new user
  */
-$app->post('/api/post-example', function() use ($app, $credentials, $requestValidator) {
-
-    $request = $app->request();
-
-    try {
-        // Validate the request signature
-        $isValid = $requestValidator->isValid($request, $credentials);
-
-        if ($isValid) {
-            $params = $request->post();
-
-            // Assume appropriate POST action of some sort, in this case saving
-            // a new user and returning the persisted user data.
-            $data = array(
-                'user' => array(
-                    'id' => 666,
-                    'name' => $params['name'],
-                    'email' => $params['email'],
-                    'department' => $params['department'],
-                ),
-            );
-
-            $jsend = new JSendResponse('success', $data);
-        } else {
-            $jsend = new JSendResponse('fail', array('message' => 'Invalid signature'));
-        }
-    } catch (\Exception $e) {
-        $jsend = new JSendResponse('error', array(), $e->getMessage());
-    }
+$app->post('/api/post-example', $validateSignature($app, $credentials, $requestValidator), function() use ($app) {
 
     $response = $app->response();
+
+    if ($response->isClientError()) {
+        return $response;
+    }
+
+    $params = $app->request()->post();
+
+    // Assume appropriate POST action of some sort, in this case saving
+    // a new user and returning the persisted user data.
+    $data = array(
+        'user' => array(
+            'id' => 666,
+            'name' => $params['name'],
+            'email' => $params['email'],
+            'department' => $params['department'],
+        ),
+    );
+
+    $jsend = new JSendResponse('success', $data);
+
+    $response['Content-Type'] = 'application/json';
+    echo $jsend->encode();
+});
+
+/**
+ * Validates a signed POST request and checks to see if this same request has
+ * been sent in the past.
+ */
+$app->post('/api/replay-example', $validateSignature($app, $credentials, $requestValidator), function() use ($app, $config) {
+
+    $response = $app->response();
+
+    if ($response->isClientError()) {
+        return $response;
+    }
+
+    try {
+        $db = new \PDO(
+            $config['pdo']['dsn'],
+            $config['pdo']['username'],
+            $config['pdo']['password'],
+            $config['pdo']['options']
+        );
+
+        $params = $app->request()->post();
+
+        $signatureDao = new SignatureDao($db);
+        $signatureDao->save($params['key'], $params['signature'], (int) gmdate('U') + 3600);
+
+        // Assume appropriate POST action of some sort, in this case saving
+        // a new user and returning the persisted user data.
+        $data = array(
+            'user' => array(
+                'id' => 666,
+                'name' => $params['name'],
+                'email' => $params['email'],
+                'department' => $params['department'],
+            ),
+        );
+
+        $jsend = new JSendResponse('success', $data);
+    } catch (\PDOException $pe) {
+        if ($pe->getCode() == 23000) {
+            $response->setStatus(403);
+            $jsend = new JSendResponse('error', array(), sprintf('REPLAYED REQUEST: %s', $pe->getMessage()), $pe->getCode());
+        }
+    } catch (\Exception $e) {
+        $response->setStatus(400);
+        $jsend = new JSendResponse('error', array(), $e->getMessage(), $e->getCode());
+    }
+
     $response['Content-Type'] = 'application/json';
     echo $jsend->encode();
 });
